9dd9120beecc14f43cce512f7ef59abd7ac506c8
MMW BOTZ <akhilkrishnaus1234@gmail.com>
2025-02-27 11:28:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,17 +14,12 @@ ADMIN = int(environ.get("ADMIN" , ""))
 CHAT_GROUP = int(environ.get("CHAT_GROUP", ""))
 LOG_CHANNEL = environ.get("LOG_CHANNEL", "")
 MONGO_URL = environ.get("MONGO_URL" , "")
-AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "")
-)
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL" , "")
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = ("STICKERS_IDS", "CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME").split()
 COOL_TIMER = 20  # keep this atleast 20
-ONLY_SCAN_IN_GRP = environ.get(
-    "ONLY_SCAN_IN_GRP", True
-)  # If IMG_SCAN_IN_GRP is set to True, image scanning is restricted to your support group only. If it's False, the image scanning feature can be used anywhere.
+ONLY_SCAN_IN_GRP = environ.get("ONLY_SCAN_IN_GRP", True)  # If IMG_SCAN_IN_GRP is set to True, image scanning is restricted to your support group only. If it's False, the image scanning feature can be used anywhere.
 REACTIONS = ["❤️‍🔥", "⚡", "🔥"]
-
 
 
 class Config(object):
